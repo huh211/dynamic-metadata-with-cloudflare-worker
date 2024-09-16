a45e5116029a5a32c551eb097fd59d2187785365
huh211 <bokunatsusoda@gmail.com>
2024-09-16 11:09:36 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 export const config = {
-  domainSource: "https://f69a71f6-9fd8-443b-a040-78beb5d404d4.weweb-preview.io", // Your WeWeb app preview link
+  domainSource: "https://8b4ecbe3-ecc3-473b-8cb3-1115ba815bc5.weweb-preview.io/", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/event/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/{id}/meta"
-      },
-      {
-          pattern: "/team/profile/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
+          pattern: "/detail0914/[^/]+",
+          metaDataEndpoint: "https://xghg-2p9b-ogfk.t7.xano.io/api:Z9ixkUX9/get_single_wamnet"
       }
-      // Add more patterns and their metadata endpoints as needed
+		// ,
+  //     {
+  //         pattern: "/team/profile/[^/]+",
+  //         metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
+  //     }
+  //     // Add more patterns and their metadata endpoints as needed
   ]
 };
