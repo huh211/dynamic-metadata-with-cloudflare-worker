4cedf6e34e0c902a77948c032ecf1bfb08886b98
huh211 <bokunatsusoda@gmail.com>
2024-10-04 16:14:56 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ export const config = {
   domainSource: "https://8b4ecbe3-ecc3-473b-8cb3-1115ba815bc5.weweb-preview.io/", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/detail0914/[^/]+",
-          metaDataEndpoint: "https://xghg-2p9b-ogfk.t7.xano.io/api:Z9ixkUX9/get_single_wamnet"
+          pattern: "/detail/[^/]+",
+          metaDataEndpoint: "https://xghg-2p9b-ogfk.t7.xano.io/api:Z9ixkUX9/get_meta_byid_0919"
       }
 		// ,
   //     {
