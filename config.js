eff6c590bb44f17bc14a1d86e3f28044c5e55d8c
huh211 <bokunatsusoda@gmail.com>
2024-10-08 11:42:07 +0900

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,5 @@ export const config = {
           pattern: "/detail/[^/]+",
           metaDataEndpoint: "https://xghg-2p9b-ogfk.t7.xano.io/api:Z9ixkUX9/get_meta_byid_0919"
       }
-		// ,
-  //     {
-  //         pattern: "/team/profile/[^/]+",
-  //         metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
-  //     }
-  //     // Add more patterns and their metadata endpoints as needed
   ]
 };
