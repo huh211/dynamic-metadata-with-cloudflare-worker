0c1d34b3dfe97037c2277ec3d7331ae363291b9d
huh211 <bokunatsusoda@gmail.com>
2024-10-13 15:15:52 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export const config = {
   domainSource: "https://8b4ecbe3-ecc3-473b-8cb3-1115ba815bc5.weweb-preview.io/", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/detai/[^/]+",
+          pattern: "/detai/[^/]+/?$",
           metaDataEndpoint: "https://xghg-2p9b-ogfk.t7.xano.io/api:Z9ixkUX9/get_meta_by/{pageid}"
       }
   ]
