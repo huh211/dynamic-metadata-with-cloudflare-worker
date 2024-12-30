9e7f4e837e2b88e4eb5a9c04af0a4630e0b24f77
huh211 <bokunatsusoda@gmail.com>
2024-12-30 14:15:16 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 export const config = {
-  domainSource: "https://8b4ecbe3-ecc3-473b-8cb3-1115ba815bc5.weweb-preview.io/", // Your WeWeb app preview link
+  domainSource: "https://iku-den.co.jp", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/detai/[^/]+/?$",
+          pattern: "/details/[^/]+",
           metaDataEndpoint: "https://xghg-2p9b-ogfk.t7.xano.io/api:Z9ixkUX9/get_meta_by/{pageid}"
       }
+      // Add more patterns and their metadata endpoints as needed
   ]
 };
